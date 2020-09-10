575afae48d7db07582d6b7e9320bded151cd053d
Amrit Saha <amritsaha607@gmail.com>
2020-09-11 00:29:17 +0530

    added-edges-visual

--- utils.py
+++ utils.py
@@ -12,7 +12,7 @@ class Circle:
         self.s = s
 
 
-def showNodes(circles, texts, mode='multi'):
+def showNodes(circles, texts, mode='multi', debug=True):
     '''
         Shows multiple nodes
     '''
@@ -23,7 +23,8 @@ def showNodes(circles, texts, mode='multi'):
         [circle.x for circle in circles], 
         [circle.y for circle in circles], 
         s=[circle.s for circle in circles], 
-        alpha=0.5
+        alpha=1,
+        zorder=2,
     )
     for i, text in enumerate(texts):
         plt.text(
@@ -32,10 +33,13 @@ def showNodes(circles, texts, mode='multi'):
             horizontalalignment='center',
             verticalalignment='center',
         )
-    plt.show()
+    if debug:
+        plt.axis('off')
+        plt.margins(0.1)
+        plt.show()
 
 
-def showCircle(circles, mode='multi'):
+def showCircle(circles, mode='multi', debug=True):
     '''
         Shows multiple circles
     '''
@@ -45,9 +49,22 @@ def showCircle(circles, mode='multi'):
         [circle.x for circle in circles], 
         [circle.y for circle in circles], 
         s=[circle.s for circle in circles], 
-        alpha=0.5
+        alpha=0.5,
+        zorder=2,
     )
-    plt.show()
+    if debug:
+        plt.show()
+
+
+def showEdges(edges, mode='multi', debug=True):
+    if not mode=='multi':
+        edges = [edges]
+    for edge in edges:
+        plt.plot(
+            [edge[0][0], edge[1][0]], [edge[0][1], edge[1][1]], 
+            c='#6699ff', 
+            zorder=1
+        )
 
 
 def makeCircles(data):
@@ -65,11 +82,11 @@ def getDepth(node):
         return 0
     return 1+max(getDepth(node.left), getDepth(node.right))
 
-def showTree(node, size=500):
+def showTree(node, edge=True, size=500, debug=True):
     d = getDepth(node)
 
     q = [[node, 0, 0, d-1]]
-    circles, texts = [], []
+    circles, texts, edges = [], [], []
 
     while len(q):
         [cur, x, y, level] = q[0]
@@ -81,7 +98,10 @@ def showTree(node, size=500):
         
         if cur.left:
             q.append([cur.left, x-2**level, y-1, level-1])
+            edges.append([(x, y), (x-2**level, y-1)])
         if cur.right:
             q.append([cur.right, x+2**level, y-1, level-1])
+            edges.append([(x, y), (x+2**level, y-1)])
 
+    showEdges(edges)
     showNodes(circles, texts, mode='multi')
